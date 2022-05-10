b193931523ebda64e26cc682c7907c3403b8774b
Uke Alex <telasoftware.contact@gmail.com>
2022-05-10 21:02:02 +0300

    Changed port number in axios

--- client/src/axios/base_url.js
+++ client/src/axios/base_url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axios_ = axios.create({
-    baseURL: 'https://rauebookstore.azurewebsites.net:5000',
+    baseURL: 'https://rauebookstore.azurewebsites.net:8080',
     headers: {
         get: {'Content-Type': 'application/json'},
         post: {'Content-Type': 'application/json'}
