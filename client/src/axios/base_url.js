81d90ba16782f1ada1cb3734bc6ec8f664055e9e
Uke Alex <telasoftware.contact@gmail.com>
2022-05-10 20:01:57 +0300

    Changed path to point to in requests

--- client/src/axios/base_url.js
+++ client/src/axios/base_url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axios_ = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://rauebookstore.azurewebsites.net:5000',
     headers: {
         get: {'Content-Type': 'application/json'},
         post: {'Content-Type': 'application/json'}
